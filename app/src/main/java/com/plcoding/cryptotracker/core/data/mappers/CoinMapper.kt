e505c2e246afa9032606521736612a8bb070fa19
Xavier Maximin <xavier.maximin.uk@gmail.com>
2024-11-04 23:11:38 -0400

    chore(networking): load coinprice
    load coinprice history

--- app/src/main/java/com/plcoding/cryptotracker/core/data/mappers/CoinMapper.kt
+++ app/src/main/java/com/plcoding/cryptotracker/core/data/mappers/CoinMapper.kt
@@ -24,6 +24,6 @@ fun CoinPriceDto.toCoinPrice(): CoinPrice {
         priceUsd = priceUsd,
         dateTime = Instant
             .ofEpochMilli(time)
-            .atZone(ZoneId.systemDefault())
+            .atZone(ZoneId.of("UTC"))
     )
 }
